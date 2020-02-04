57a25d4a80f89ca9aaa8af891ce4d11122e4026b
vicyor <2457569580@qq.com>
2020-02-04 13:41:01 +0800

    添加异步配置

---
+++ src/main/java/com/vicyor/zookeeper/Master.java
@@ -0,0 +1,8 @@
+package com.vicyor.zookeeper;
+
+/**
+ * 作者:姚克威
+ * 时间:2020/2/4 12:23
+ **/
+public class Master {
+}
---
+++ src/main/java/com/vicyor/zookeeper/async/Master.java
@@ -0,0 +1,8 @@
+package com.vicyor.zookeeper.async;
+
+/**
+ * 作者:姚克威
+ * 时间:2020/2/4 13:10
+ **/
+public class Master {
+}
